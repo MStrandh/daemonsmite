88d44a5629bdaa4e99d6016ab69f94c956b0c75d
Magnus Strandh <magnus.strandh@king.com>
2018-03-01 13:27:20 +0100

    CCS styling of page

--- dev/js/DaemonSmiteApp.js
+++ dev/js/DaemonSmiteApp.js
@@ -2,3 +2,5 @@ DaemonConsole.setOutput($("#console"));
 
 DaemonConsole.log("Welcome!");
 DaemonConsole.log("Do you want to play a game?");
+
+var game = new DaemonSmiteGame();
---
+++ dev/js/DaemonSmiteGame.js
@@ -0,0 +1,5 @@
+class DaemonSmiteGame {
+	constructor() {
+		console.log("Here");
+	}
+}
